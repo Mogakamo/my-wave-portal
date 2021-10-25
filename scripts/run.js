2b8a8a6f1d569e19f44250f3aac8ae37b65724c9
Mogakamo <mogaka.amo254@gmail.com>
2021-10-25 12:11:14 +0000

    able to store data to the blockchain environment

--- scripts/run.js
+++ scripts/run.js
@@ -3,18 +3,33 @@ const main = async () => {
   const waveContractFactory = await hre.ethers.getContractFactory("WavePortal");
   const waveContract = await waveContractFactory.deploy();
   await waveContract.deployed();
-  
-  
+
+  //creating the array to store addresses of the people who have waved.
+  const addressesArray = [];
+
+  //store the Addresses in the empty array
+  for (let i = 0; i < addressesArray.length; i++) {
+    addressesArray.push(randomPerson.getTotalWaves);
+  }
+
   console.log("Contract deployed to:", waveContract.address);
-  console.log("Contract deployed by:", owner.address)
+  console.log("Contract deployed by:", owner.address);
 
   let waveCount;
   waveCount = await waveContract.getTotalWaves();
 
-  const waveTxn = await waveContract.wave();
+  let waveTxn = await waveContract.wave();
   await waveTxn.wait();
 
   waveCount = await waveContract.getTotalWaves();
+
+  waveTxn = await waveContract.connect(randomPerson).wave();
+  await waveTxn.wait();
+
+  waveCount = await waveContract.getTotalWaves();
+
+  //display the added addresses
+  console.log(addressesArray);
 };
 
 const runMain = async () => {
